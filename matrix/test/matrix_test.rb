0dac36783608fe9fdb8452f6694eb6329cf1d269
Anson Li <ansonli.94@gmail.com>
2016-01-16 18:56:50 -0700

    matrix demo build & test

--- matrix/test/matrix_test.rb
+++ matrix/test/matrix_test.rb
@@ -4,6 +4,28 @@ require "matrix"
 # test with bundle exec rake
 class UrlTest < Minitest::Test
   def test_url
+    assert_equal "https://github.com/Team-Aqua/Matrix-Library/", TwoDMatrix::Url
     assert_equal "https://github.com/Team-Aqua/Matrix-Library/", Matrix::Url
   end
+end
+
+class DemoTest < Minitest::Test
+	def setup 
+		@matrix = TwoDMatrix.new
+	end
+
+	def test_demo_show
+		multiarray = [[1,2,3], [1,2,3]]
+		puts "**Demo Intro**"
+		puts @matrix.demo_show(multiarray)
+		puts "**End demo intro**"
+	end
+
+	def test_demo_by_index
+		multiarray = [[1,2,3], [1,2,3]]
+		puts "**Demo by Index**"
+		puts @matrix.demo_by_index(multiarray)
+		puts "**End demo by index**"
+	end
+
 end
