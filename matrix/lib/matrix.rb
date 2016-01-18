172f7182a49ddb24394af1386bfc30a538b8c39a
Anson Li <ansonli.94@gmail.com>
2016-01-18 12:17:30 -0700

    syntax cleanup

--- matrix/lib/matrix.rb
+++ matrix/lib/matrix.rb
@@ -18,14 +18,59 @@ class TwoDMatrix
 	end
 
 	# Builds when given a 2d array - to be true CSR conversion
-	def build(array) # assume this array is 2d eg. [0 0 2] [1 0 2] [1 0 0]
+	def build_from_array(array) # assume this array is 2d eg. [0 0 2] [1 0 2] [1 0 0]
 		if depth(array) == 2 # 2d verification
 			puts "Array dim is correct.\nBuilding CSR format."
 			# sample design
 			# 1. set col_ind = 0 (0/1/2), row_ptr = 0
 			# 2. identify the dimensions of the array (3x3, 2x4, etc.) store row_val = row# and col_val = col#
+			# 3. check the first nonzero point and check its location; fill as necessary.
+			# 4. repeat and clean
 		end
 	end	
+
+	# Builds array using user-generated CSR values
+	def build_from_csr(row_ptr, col_ind, val, col_siz, row_siz)
+		# generate 
+	end
+
+	# Identifies the 'column' value of an array (eg. the number of entries in a column)
+	# Todo: optimize system for O(cn), if possible
+	def max_col(array)
+		values = array
+		max_count = 0
+		# Loop over indexes.
+		values.each_index do |i|
+			counter = 0
+		  # Get subarray and loop over its indexes also.
+		  subarray = values[i]
+		  subarray.each_index do |x|
+		  	counter += 1
+		  end
+		  if counter > max_count
+		  	max_count = counter
+		  end
+		end
+		return max_count
+	end
+
+	# Identifies the 'row' value of an array (eg. the number of entries in a row)
+	def max_row(array)
+		values = array
+		max_count = 0
+		# Loop over indexes.
+		values.each_index do |i|
+		  # Get subarray and loop over its indexes also.
+		  max_count += 1
+		end
+		return max_count
+	end
+
+	def dimensions(array)
+		column = max_col(array)
+		row = max_row(array)
+		puts "Dimensions, by column x row, are #{column} x #{row}"
+	end
 
 	# Code taken from http://stackoverflow.com/questions/9545613/getting-dimension-of-multidimensional-array-in-ruby
 	def depth(array)
