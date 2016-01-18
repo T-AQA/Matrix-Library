732c2ccd0e334d35500613c1f890152ebff3c8c0
Anson Li <ansonli.94@gmail.com>
2016-01-18 14:29:46 -0700

    refactoring

--- matrix/test/matrix_test.rb
+++ matrix/test/matrix_test.rb
@@ -13,36 +13,22 @@ class DemoTest < Minitest::Test
 	def setup 
 		@matrix = TwoDMatrix.new
 
-        @matrixDense3x3 = TwoDMatrix.new
-        @matrixDense3x3.build_from_array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+    @matrixDense3x3 = TwoDMatrix.new
+    @matrixDense3x3.build_from_array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
 
-        @matrixSparse3x3 = TwoDMatrix.new
-        @matrixSparse3x3.build_from_array([[0, 2, 0], [1, 0, 0], [0, 2, 0]])
+    @matrixSparse3x3 = TwoDMatrix.new
+    @matrixSparse3x3.build_from_array([[0, 2, 0], [1, 0, 0], [0, 2, 0]])
 
-        @matrixTrigonal3x3 = TwoDMatrix.new
-        @matrixTrigonal3x3.build_from_array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    @matrixTrigonal3x3 = TwoDMatrix.new
+    @matrixTrigonal3x3.build_from_array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 	end
 
-	def test_demo_show
-		multiarray = [[1,2,3], [1,2,3]]
-		puts "**Demo Intro**"
-		puts @matrix.demo_show(multiarray)
-		puts "**End demo intro**"
-	end
+  def test_build_from_array
+      @matrix.build_from_array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+  end
 
-	def test_demo_by_index
-		multiarray = [[1,2,3], [1,2,3]]
-		puts "**Demo by Index**"
-		puts @matrix.demo_by_index(multiarray)
-		puts "**End demo by index**"
-	end
-
-    def test_build_from_array
-        @matrix.build_from_array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
-    end
-
-    def test_dimensions
-        #assert_equal @matrixSparse3x3.dimensions(), [3, 3]
-    end
+  def test_dimensions
+      #assert_equal @matrixSparse3x3.dimensions(), [3, 3]
+  end
 
 end
