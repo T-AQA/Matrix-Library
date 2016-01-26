2939468a050fe38545390c00a137cdb589d7f039
Main User Account <anplamon@ualberta.ca>
2016-01-26 11:32:26 -0700

    added tests for eigensystem decomposition. added possibility for LUP decomposition tests.

--- matrix/test/matrix_test.rb
+++ matrix/test/matrix_test.rb
@@ -215,3 +215,37 @@ class PropertiesTest < Minitest::Test
   end
 
 end 
+
+class DecompositionsTest < Minitest::Test
+
+  def setup
+    @matrix = TwoDMatrix.new
+
+    @matrixDense3x3 = TwoDMatrix.new
+    @matrixDense3x3.build_from_array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+
+    @matrixSparse3x3 = TwoDMatrix.new
+    @matrixSparse3x3.build_from_array([[0, 1, 0], [2, 0, 0], [0, 0, 3]])
+  end
+
+	#alias for eigensystem
+	#returns a list in the form {eigenvalues, eigenvectors}
+  def test_eigen
+    assert_equals @matrixDense3x3.eigen(), [[6, 0, 0], [1, 1, 1], [-3, 0, 1], [-2, 1, 0]]
+  end
+
+	def test_eigensystem
+    assert_equals @matrixDense3x3.eigensystem(), [[6, 0, 0], [1, 1, 1], [-3, 0, 1], [-2, 1, 0]]
+  end
+
+	#alias for lup_decomposition
+	#Need to look into this for output http://www.wolframalpha.com/input/?i=%5B%5B0%2C+1%2C+0%5D%2C+%5B2%2C+0%2C+0%5D%2C+%5B0%2C+0%2C+3%5D%5D+LU+decomposition
+	def test_lup
+    #assert @matrixSparse3x3.lup(), [L, U, P]
+  end
+
+	def test_lup_decomposition
+    #assert @matrixSparse3x3.lup_decomposition(), [L, U, P]
+  end
+
+end
