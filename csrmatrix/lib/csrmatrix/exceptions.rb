e1a2161c1fde7919254841b9f14b83589d9cf1fe
Quentin Lautischer <lautisch@ualberta.ca>
2016-02-01 17:23:36 -0700

    Fixed/Added Exception Module

---
+++ csrmatrix/lib/csrmatrix/exceptions.rb
@@ -0,0 +1,13 @@
+module CsrMatrix
+    module Exceptions
+        class MatrixDimException < StandardError; end
+        class NullMatrixException < StandardError; end     
+        class MatrixDimException < StandardError; end      
+        class MatrixDimException < StandardError; end      
+        class ArithmeticException < StandardError; end     
+        class MatrixTypeException < StandardError; end     
+        class DivideByZeroException < StandardError; end   
+        class IndexOutOfRangeException < StandardError; end
+        class ArgumentNullException < StandardError; end   
+    end
+end
