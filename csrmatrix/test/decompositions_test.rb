1abfc56664e784488a30b6b38337a78babfd8bbb
Main User Account <anplamon@ualberta.ca>
2016-02-02 14:35:07 -0700

    Fixed test case for LUP decomp.

--- csrmatrix/test/decompositions_test.rb
+++ csrmatrix/test/decompositions_test.rb
@@ -9,9 +9,6 @@ class DecompositionsTest < Minitest::Test
     @matrixDense3x3 = TwoDMatrix.new
     @matrixDense3x3.build_from_array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
 
-    @matrixSparse3x3 = TwoDMatrix.new
-    @matrixSparse3x3.build_from_array([[0, 1, 0], [2, 0, 0], [0, 0, 3]])
-
 		@matrixLUP3x3 = TwoDMatrix.new
 		@matrixLUP3x3.build_from_array([[1, 2, 4], [3, 8, 14], [2, 6, 13]])
   end
@@ -30,11 +27,11 @@ class DecompositionsTest < Minitest::Test
 	#returns a list of matrices {L, U}
 	#Reference: http://nucinkis-lab.cc.ic.ac.uk/HELM/workbooks/workbook_30/30_3_lu_decomposition.pdf
 	def test_lup
-    assert_equal [[[1, 0, 0], [3, 1, 0], [2, 1, 1]], [[1, 2, 4], [0, 2, 2], [0, 0, 3]]], @matrixSparse3x3.lup()
+    assert_equal [[[1, 0, 0], [3, 1, 0], [2, 1, 1]], [[1, 2, 4], [0, 2, 2], [0, 0, 3]]], @matrixLUP3x3.lup()
   end
 
 	def test_lup_decomposition
-    assert_equal [[[1, 0, 0], [3, 1, 0], [2, 1, 1]], [[1, 2, 4], [0, 2, 2], [0, 0, 3]]], @matrixSparse3x3.lup_decomposition()
+    assert_equal [[[1, 0, 0], [3, 1, 0], [2, 1, 1]], [[1, 2, 4], [0, 2, 2], [0, 0, 3]]], @matrixLUP3x3.lup_decomposition()
   end
 
 end
