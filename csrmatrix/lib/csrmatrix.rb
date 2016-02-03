ddba21fad3bfe8d4627f426f24ced23707d9cb11
Anson Li <ansonli.94@gmail.com>
2016-02-03 16:56:12 -0700

    init dev

--- csrmatrix/lib/csrmatrix.rb
+++ csrmatrix/lib/csrmatrix.rb
@@ -45,11 +45,17 @@ class TwoDMatrix
 
   # equals override for matrix operations
   def ==(o)
+    # equals overide to check if object o equals self
+    # pre   o, self
+    # post  true if o is_a csrmatrix and o == self
     o.class == self.class && o.state == state
   end
 
   # FIXME: convert to protected value
   def state
+    # returns the current state of the csrmatrix
+    # pre self
+    # post [@value, @row_pointer, @ column_index, @rows, @columns, @dimension]
     [@val, @row_ptr, @col_ind, @rows, @columns, @ndim]
   end
 
