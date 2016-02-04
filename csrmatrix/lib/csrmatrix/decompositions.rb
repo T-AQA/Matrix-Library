2b6d1eac93d4b7f72797cfdfdfa0f4325356ad0a
Anson Li <ansonli.94@gmail.com>
2016-02-03 17:17:30 -0700

    two more down the drain

--- csrmatrix/lib/csrmatrix/decompositions.rb
+++ csrmatrix/lib/csrmatrix/decompositions.rb
@@ -1,13 +1,18 @@
 module CsrMatrix
 	module Decompositions
 
-			#alias for eigensystem
-			#returns a list in the form {eigenvalues, eigenvectors}
 			def eigen()
+				# alias for eigensystem
+				# returns a list in the form {eigenvalues, eigenvectors}
+				# pre 	existing matrix
+				# post 	eigenvalues of the matrix
     		eigenvalue()
 			end
         
 			def eigenvalue()
+				# identifies the eigenvalues of a matrix
+				# pre 	existing matrix
+				# post 	eigenvalues of the matrix
 				m = Matrix.rows(self.decompose)
 				return m.eigensystem().to_a[1].round().to_a
 			end
--- csrmatrix/lib/csrmatrix/functions.rb
+++ csrmatrix/lib/csrmatrix/functions.rb
@@ -2,31 +2,50 @@ module CsrMatrix
     module Functions
         
         def determinant()
+            # identifies the determinant of a matrix
+            # pre   existing matrix
+            # post  determinant of the matrix
             m = Matrix.rows(self.decompose)
             m.det()
         end
 
-				#alias for determinant
         def det()
+            # alias for determinant
+            # identifies the determinant of a matrix
+            # pre   existing matrix
+            # post  determinant of the matrix
             self.determinant()
         end
 
         def rank()
+            # identifies the rank of a matrix
+            # pre   existing matrix
+            # post  rank of the matrix
             m = Matrix.rows(self.decompose)
             m.rank()
         end
 
         def round(ndig = 0)
+            # identifies the round of a matrix (that is, each value rounded by a specific degree)
+            # pre   integer of degree, existing matrix
+            # post  rounded array
             m = Matrix.rows(self.decompose)
             self.build_from_array(m.round(ndig).to_a())
         end
 
         def trace()
+            # identifies the trace of the matrix
+            # pre   existing matrix
+            # post  trace
             m = Matrix.rows(self.decompose)
             m.trace()
         end
 
         def tr()
+            # alias for trace
+            # identifies the trace of the matrix
+            # pre   existing matrix
+            # post  trace
             self.trace()
         end
 
