67699068db694118949292bf9cb0a8b95a8c4aba
Anson Li <ansonli.94@gmail.com>
2016-02-07 14:18:53 -0700

    tmp functions build

--- csrmatrix/lib/csrmatrix/functions.rb
+++ csrmatrix/lib/csrmatrix/functions.rb
@@ -1,52 +1,60 @@
 module CsrMatrix
     module Functions
-        
+        include Contracts::Core
+        include Contracts::Builtin
+
+        Contract Contracts::Send[:not_null?] => Contracts::Num
         def determinant()
             # identifies the determinant of a matrix
             # pre   existing matrix
             # post  determinant of the matrix
             m = Matrix.rows(self.decompose)
-            m.det()
+            return m.det()
         end # determinant
 
+        Contract Contracts::Send[:not_null?] => Contracts::Num
         def det()
             # alias for determinant
             # identifies the determinant of a matrix
             # pre   existing matrix
             # post  determinant of the matrix
-            self.determinant()
+            return self.determinant()
         end # det
 
+        Contract Contracts::Send[:not_null?] => Contracts::Num
         def rank()
             # identifies the rank of a matrix
             # pre   existing matrix (matrix.not_null?)
             # post  rank of the matrix
             m = Matrix.rows(self.decompose)
-            m.rank()
+            return m.rank()
         end # rank
 
+        Contract Contracts::Num => Contracts::Bool
         def round(ndig = 0)
             # identifies the round of a matrix (that is, each value rounded by a specific degree)
             # pre   integer of degree, existing matrix (matrix.not_null?)
             # post  rounded array
             m = Matrix.rows(self.decompose)
-            self.build_from_array(m.round(ndig).to_a())
+            return self.build_from_array(m.round(ndig).to_a())
         end # round
 
+        Contract Contracts::Send[:not_null?] => Contracts::Num
         def trace()
             # identifies the trace of the matrix
             # pre   existing matrix (matrix.not_null?)
             # post  trace
             m = Matrix.rows(self.decompose)
-            m.trace()
+            return m.trace()
         end # trace
 
+        Contract Contracts::Send[:not_null?] => Contracts::Num
         def tr()
             # alias for trace
             # identifies the trace of the matrix
             # pre   existing matrix (matrix.not_null?)
             # post  trace
-            self.trace()
+            return self.trace()
         end # tr
 
     end # Functions
